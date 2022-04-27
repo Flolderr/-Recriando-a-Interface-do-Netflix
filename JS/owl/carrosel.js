c4aeb39e454685f2c8c35a56a7c397dec16c1c29
flolderr <flolderr@gmail.com>
2022-04-26 23:44:23 -0300

    desenvolvimento final, aula recriando a pagina do netflix

--- JS/owl/carrosel.js
+++ JS/owl/carrosel.js
@@ -1,16 +1,39 @@
+/*
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:8,
+    loop:true,
+    margin:05,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[5000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+*/
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    
+    autoplay:true,
+    autoplayTimeout:5000,
+    
+    margin:05,
     nav:true,
+    
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:4
         },
         1000:{
-            items:5
+            items:7
         }
     }
 })
